de719e1c6dc6d5a28d2a26082122c19423393ac2
Guillermo1804 <memosanchez101@gmail.com>
2026-02-14 23:18:52 -0600

    Configuración Basica Pryecto

---
+++ GTEA-Proyect-WebApp/src/app/screens/registro-screen/registro-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-screen',
+  imports: [],
+  templateUrl: './registro-screen.component.html',
+  styleUrl: './registro-screen.component.scss'
+})
+export class RegistroScreenComponent {
+
+}
