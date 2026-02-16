e1fa1f2b418c05cccc9b9e5003acad24717c9c08
MadManJohnSmith <alandanielsalas@gmail.com>
2026-02-15 22:14:51 -0600

    feat: Frontend registro

--- GTEA-Proyect-WebApp/src/app/app.routes.ts
+++ GTEA-Proyect-WebApp/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { RegistroScreenComponent } from './screens/registro-screen/registro-screen.component';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component'
 
 export const routes: Routes = [
     {path: 'registro', component: RegistroScreenComponent},
+    { path: 'login', component: LoginScreenComponent },
     {path: '', redirectTo: 'registro', pathMatch: 'full'},
 ];
