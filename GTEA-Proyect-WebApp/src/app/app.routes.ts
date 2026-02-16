367c528d7ac7225cb9c62b63584c6427b0771ae1
MadManJohnSmith <alandanielsalas@gmail.com>
2026-02-15 18:33:51 -0600

    Inicio rama

--- GTEA-Proyect-WebApp/src/app/app.routes.ts
+++ GTEA-Proyect-WebApp/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RegistroScreenComponent } from './screens/registro-screen/registro-screen.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'registro', component: RegistroScreenComponent},
+    {path: '', redirectTo: 'registro', pathMatch: 'full'},
+];
